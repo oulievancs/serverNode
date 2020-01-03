85928c63dbd4572413de831bb1fecf1759e04f3f
cs151051 <cs151051@uniwa.gr>
2020-01-03 16:29:21 +0200

    REST API v1.1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn serv:app --timeout 150000
---
+++ serv.py
@@ -0,0 +1,59 @@
+#Authors:   Oulis Evangelos, Oulis Nikolaos, Drosos Katsibras
+#===================================================================
+# using flask restful
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from json import dumps
+import json
+from flask_cors import CORS
+
+# ==================================================================
+# ==================================================================
+
+# creating the flask app
+app = Flask(__name__)
+CORS(app)
+
+# creating an API object
+api = Api(app)
+
+parks = dict()
+
+# ==================================================================
+# making a class for a particular resource 
+# the get, post methods correspond to get and post requests 
+# they are automatically mapped by flask_restful. 
+# other methods include put, delete, etc.
+class Parking(Resource):
+    def get(self):
+        return parks, 200
+
+class ParkingStatus(Resource):
+    def get(self):
+        return """<html>
+            <head><title>ERROR</title></head>
+            <body><h1>Not get at '/parkingStatus'.</h1></body>
+            </html>"""
+    def post(self):
+        print (request)
+        data = json.loads(request.data)
+        print (data)
+        parks[data['no']] = data['status']
+        return parks[data['no']], 201
+
+
+# ==================================================================
+# adding the defined resources along with their corresponding urls 
+api.add_resource(Parking, '/')
+api.add_resource(ParkingStatus, '/parkingStatus')
+
+# ==================================================================
+# driver function
+if __name__ == '__main__':
+    app.run(
+            debug=True,
+            host=app.config.get("HOST", "0.0.0.0"),
+            port=app.config.get("PORT", "8080")
+    )
+
+# END
